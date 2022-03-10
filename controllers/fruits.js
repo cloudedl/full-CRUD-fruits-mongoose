19ffd2a076a6c87fd8ae95f250bc63e551984872
cloud3999 <edwardlcloud@gmail.com>
2022-03-10 10:39:59 -0500

    refactored to use controllers and express router

---
+++ controllers/fruits.js
@@ -0,0 +1,156 @@
+////////////////////////////////////////////
+// Import Dependencies
+////////////////////////////////////////////
+const express = require('express')
+const Fruit = require('../models/fruit')
+
+////////////////////////////////////////////
+// Create router
+////////////////////////////////////////////
+const router = express.Router()
+
+
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
+
+router.get('/seed', (req, res) => {
+    // arr of starter fruits
+    const startFruits = [
+        { name: 'Orange', color: 'orange', readyToEat: false },
+        { name: 'Grape', color: 'purple', readyToEat: false },
+        { name: 'Banana', color: 'orange', readyToEat: false },
+        { name: 'Strawberry', color: 'red', readyToEat: false },
+        { name: 'Coconut', color: 'brown', readyToEat: false }
+	]
+
+    // when we seed data, there are a few steps involved
+    // delete all the data that already exists(will only happen if data exists)
+    Fruit.remove({})
+        .then(data => {
+            console.log('this is what remove returns', data)
+            // then we create with our seed data
+            Fruit.create(startFruits)
+                .then(data => {
+                    console.log('this is what create returns', data)
+                    res.send(data)
+                })
+        })
+    // then we can send if we want to see that data
+})
+
+// INDEX ROUTE
+router.get('/', (req, res) => {
+    // find the fruits
+    Fruit.find({})
+        // then render a template AFTER they're found
+        .then(fruits => {
+            console.log(fruits)
+            res.render('fruits/index.liquid', { fruits })
+        })
+        // show an error if there is one
+        .catch(error => {
+            console.log(error)
+            res.json({ error })
+        })
+})
+
+// new route -> GET route that renders our page with the form
+router.get('/new', (req, res) => {
+    res.render('fruits/new')
+})
+
+//CREATE ROUTE -> POST route that actually calls the db and makes a new document
+router.post('/', (req,res) => {
+    console.log('this is the fruit to create', req.body)
+
+    //check and set readyToEat into one line of code
+    req.body.readyToEat = req.body.readyToEat === 'on' ? true : false
+    console.log
+   
+    //now we're ready for mongoose to do its thing
+    Fruit.create(req.body)
+        .then(data => {
+            console.log('this was returned from create', data)
+            res.redirect('/fruits')
+        })
+        .catch(err => {
+            console.log(err)
+            res.json(err)
+        })
+})
+
+//EDIT ROUTE
+router.get('/:id/edit', (req,res) => {
+    // we need to get the Id
+    const fruitId = req.params.id
+    //find the fruit
+    Fruit.findById(fruitId)
+    //--render if there is a fruit
+        .then(fruit => {
+            res.render('fruits/edit.liquid', {fruit})
+        })
+    // error if no fruit
+        .catch(err => {
+            console.log(err)
+            res.json(err)
+        })
+})
+// UPDATE ROUTE
+router.put('/:id', (req,res) => {
+    //get the id
+    const fruitId = req.params.id
+    //check and assign ready to eat property with correct value
+    req.body.readyToEat = req.body.readyToEat === 'on' ? true : false
+
+    //tell mongoose to update the fruit
+    Fruit.findByIdAndUpdate(fruitId, req.body, {new: true})
+        .then(fruit => {
+            console.log('the updated fruit', fruit)
+            res.redirect(`/fruits/${fruit.id}`)
+        })
+          // error if no fruit
+          .catch(err => {
+            console.log(err)
+            res.json(err)
+        })
+})
+
+// SHOW ROUTE
+router.get('/:id', (req,res) => {
+    // first, we need to the id
+    const fruitId = req.params.id
+    // then we can find a fruit by it's id
+    Fruit.findById(fruitId)
+    // once found, we can render a view with the data
+        .then(fruit => {
+            res.render('fruits/show.liquid', {fruit})
+        })
+    // if there is an error, show that instead
+        .catch(err => {
+            console.log(err)
+            res.json({ err })
+        })
+})
+
+
+//DELETE ROUTE
+router.delete('/:id', (req,res) => {
+    //get fruit id
+    const fruitId =req.params.id
+    // delete the fruit
+    Fruit.findByIdAndRemove(fruitId)
+        .then(fruit => {
+            console.log('this is the response from FBID', fruit)
+            res.redirect('/fruits')
+        })
+        .catch(err => {
+            console.log(err)
+            res.json({ err })
+        })
+
+})
+////////////////////////////////////////////
+// Export Router
+////////////////////////////////////////////
+module.exports = router
--- server.js
+++ server.js
@@ -6,7 +6,9 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
-const Fruit = require('./models/fruit')
+
+//now that we are using controllers we need to require our Router
+const FruitRouter = require('./controllers/fruits')
 
 ////////////////////////////////////////////
 // Create our express application object
@@ -27,145 +29,12 @@ app.use(express.static('public'))
 ////////////////////////////////////////////
 // Routes
 ////////////////////////////////////////////
+
+//send all '/fruits' routes to the FruitRouter
+app.use('/fruits', FruitRouter)
+
 app.get('/', (req, res) => {
     res.send('your server is running, better go catch it')
-})
-
-app.get('/fruits/seed', (req, res) => {
-    // arr of starter fruits
-    const startFruits = [
-        { name: 'Orange', color: 'orange', readyToEat: false },
-        { name: 'Grape', color: 'purple', readyToEat: false },
-        { name: 'Banana', color: 'orange', readyToEat: false },
-        { name: 'Strawberry', color: 'red', readyToEat: false },
-        { name: 'Coconut', color: 'brown', readyToEat: false }
-	]
-
-    // when we seed data, there are a few steps involved
-    // delete all the data that already exists(will only happen if data exists)
-    Fruit.remove({})
-        .then(data => {
-            console.log('this is what remove returns', data)
-            // then we create with our seed data
-            Fruit.create(startFruits)
-                .then(data => {
-                    console.log('this is what create returns', data)
-                    res.send(data)
-                })
-        })
-    // then we can send if we want to see that data
-})
-
-// INDEX ROUTE
-app.get('/fruits', (req, res) => {
-    // find the fruits
-    Fruit.find({})
-        // then render a template AFTER they're found
-        .then(fruits => {
-            console.log(fruits)
-            res.render('fruits/index.liquid', { fruits })
-        })
-        // show an error if there is one
-        .catch(error => {
-            console.log(error)
-            res.json({ error })
-        })
-})
-
-// new route -> GET route that renders our page with the form
-app.get('/fruits/new', (req, res) => {
-    res.render('fruits/new')
-})
-
-//CREATE ROUTE -> POST route that actually calls the db and makes a new document
-app.post('/fruits', (req,res) => {
-    console.log('this is the fruit to create', req.body)
-
-    //check and set readyToEat into one line of code
-    req.body.readyToEat = req.body.readyToEat === 'on' ? true : false
-    console.log
-   
-    //now we're ready for mongoose to do its thing
-    Fruit.create(req.body)
-        .then(data => {
-            console.log('this was returned from create', data)
-            res.redirect('/fruits')
-        })
-        .catch(err => {
-            console.log(err)
-            res.json(err)
-        })
-})
-
-//EDIT ROUTE
-app.get('/fruits/:id/edit', (req,res) => {
-    // we need to get the Id
-    const fruitId = req.params.id
-    //find the fruit
-    Fruit.findById(fruitId)
-    //--render if there is a fruit
-        .then(fruit => {
-            res.render('fruits/edit.liquid', {fruit})
-        })
-    // error if no fruit
-        .catch(err => {
-            console.log(err)
-            res.json(err)
-        })
-})
-// UPDATE ROUTE
-app.put('/fruits/:id', (req,res) => {
-    //get the id
-    const fruitId = req.params.id
-    //check and assign ready to eat property with correct value
-    req.body.readyToEat = req.body.readyToEat === 'on' ? true : false
-
-    //tell mongoose to update the fruit
-    Fruit.findByIdAndUpdate(fruitId, req.body, {new: true})
-        .then(fruit => {
-            console.log('the updated fruit', fruit)
-            res.redirect(`/fruits/${fruit.id}`)
-        })
-          // error if no fruit
-          .catch(err => {
-            console.log(err)
-            res.json(err)
-        })
-})
-
-// SHOW ROUTE
-app.get('/fruits/:id', (req,res) => {
-    // first, we need to the id
-    const fruitId = req.params.id
-    // then we can find a fruit by it's id
-    Fruit.findById(fruitId)
-    // once found, we can render a view with the data
-        .then(fruit => {
-            res.render('fruits/show.liquid', {fruit})
-        })
-    // if there is an error, show that instead
-        .catch(err => {
-            console.log(err)
-            res.json({ err })
-        })
-})
-
-
-//DELETE ROUTE
-app.delete('/fruits/:id', (req,res) => {
-    //get fruit id
-    const fruitId =req.params.id
-    // delete the fruit
-    Fruit.findByIdAndRemove(fruitId)
-        .then(fruit => {
-            console.log('this is the response from FBID', fruit)
-            res.redirect('/fruits')
-        })
-        .catch(err => {
-            console.log(err)
-            res.json({ err })
-        })
-
 })
 
 
